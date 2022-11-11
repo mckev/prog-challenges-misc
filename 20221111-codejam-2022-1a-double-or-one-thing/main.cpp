4283488fc4fa2825799bc1577f3414f9eff44436
Kevin Haritmonds <haritmonds@yahoo.com>
2022-11-11 14:21:59 +0530

    Google Code Jam 2022 Round 1A : Double or One Thing

---
+++ 20221111-codejam-2022-1a-double-or-one-thing/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+std::string solve(const std::string& S) {
+    std::string output;
+    std::string pocket;
+    for (int i = 0; i < S.size(); i++) {
+        output += S[i];
+        if (i + 1 >= S.size()) continue;
+        if (S[i + 1] < S[i]) {
+            // Case descending : Do not repeat
+            pocket = "";
+        } else if (S[i + 1] > S[i]) {
+            // Case ascending : Repeat
+            output += pocket + S[i];
+            pocket = "";
+        } else if (S[i + 1] == S[i]) {
+            pocket += S[i];
+        }
+    }
+    return output;
+}
+
+int main() {
+    int T;
+    std::cin >> T;
+    for (int t = 0; t < T; t++) {
+        std::string S; std::cin >> S;
+        std::cout << "Case #" << t + 1 << ": " << solve(S) << std::endl;
+    }
+    // std::cout << solve("PEEL") << std::endl;
+    // std::cout << solve("AAAAAAAAAA") << std::endl;
+    // std::cout << solve("CODEJAMDAY") << std::endl;
+    // std::cout << solve("HELLOWORLD") << std::endl;
+    return 0;
+}
