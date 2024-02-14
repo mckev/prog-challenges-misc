0c9976b01b1f9eaef854e10dc3eac2455513a51b
KHaritmonds <kharitmonds@slb.com>
2024-02-14 20:08:40 +0530

    LeetCode : LRU Cache

--- 20240214-leetcode-lru-cache/main.cpp
+++ 20240214-leetcode-lru-cache/main.cpp
@@ -69,10 +69,10 @@ int main() {
         int value;
         LRUCache lRUCache = LRUCache(2);
         lRUCache.put(1, 0);                     // cache is {1=0}
-        lRUCache.put(2, 2);                     // cache is {1=1, 2=2}
+        lRUCache.put(2, 2);                     // cache is {1=0, 2=2}
         value = lRUCache.get(1);                // return 0
         assert(value == 0);
-        lRUCache.put(3, 3);                     // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        lRUCache.put(3, 3);                     // LRU key was 2, evicts key 2, cache is {1=0, 3=3}
         value = lRUCache.get(2);                // returns -1 (not found)
         assert(value == LRUCache::NOT_FOUND);
         lRUCache.put(4, 4);                     // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
