b5c408b346d38edd75a8f4d20a56f51cc2458dcf
Kevin Haritmonds <haritmonds@yahoo.com>
2024-03-11 21:42:04 +0530

    SPOJ : Prata : Using priority queue for job tracking

--- easy/20240311-spoj-prata/main.cpp
+++ easy/20240311-spoj-prata/main.cpp
@@ -16,17 +16,18 @@ struct Job {
     }
 };
 
+const int MAX_COOKS = 8;
 
-int solve(int num_pratas, const std::vector<int>& num_ranks) {
+int solve(int target, const std::vector<int>& num_ranks) {
     std::priority_queue<Job> jobs;
-    for (int rank = 1; rank <= 8; rank++) {
+    for (int rank = 1; rank <= MAX_COOKS; rank++) {
         if (num_ranks[rank] > 0) {
             jobs.push({rank, rank, num_ranks[rank], 2});
         }
     }
     int completed = 0;
     int time = 0;
-    while (completed < num_pratas) {
+    while (completed < target) {
         Job job = jobs.top(); jobs.pop();
         completed += job.num_cooks;
         time = job.time;
@@ -37,24 +38,24 @@ int solve(int num_pratas, const std::vector<int>& num_ranks) {
 
 
 int main() {
-    // {
-    //     const int UNDEFINED = -999;
-    //     assert(solve(1000, std::vector<int>{UNDEFINED, 0, 0, 0, 0, 0, 0, 0, 1}) == 4004000);
-    //     assert(solve(10, std::vector<int>{UNDEFINED, 1, 1, 1, 1, 0, 0, 0, 0}) == 12);
-    //     assert(solve(8, std::vector<int>{UNDEFINED, 1, 0, 0, 0, 0, 0, 0, 0}) == 36);
-    //     assert(solve(8, std::vector<int>{UNDEFINED, 8, 0, 0, 0, 0, 0, 0, 0}) == 1);
-    // }
+    {
+        const int UNDEFINED = -999;
+        assert(solve(1000, std::vector<int>{UNDEFINED, 0, 0, 0, 0, 0, 0, 0, 1}) == 4004000);
+        assert(solve(10, std::vector<int>{UNDEFINED, 1, 1, 1, 1, 0, 0, 0, 0}) == 12);
+        assert(solve(8, std::vector<int>{UNDEFINED, 1, 0, 0, 0, 0, 0, 0, 0}) == 36);
+        assert(solve(8, std::vector<int>{UNDEFINED, 8, 0, 0, 0, 0, 0, 0, 0}) == 1);
+    }
     int T; std::cin >> T;
     for (int t = 0; t < T; t++) {
-        int num_pratas; std::cin >> num_pratas;
-        std::vector<int> num_ranks(9);
+        int target; std::cin >> target;
+        std::vector<int> num_ranks(MAX_COOKS + 1);
         int num_cooks; std::cin >> num_cooks;
         for (int i = 0; i < num_cooks; i++) {
             int rank;
             std::cin >> rank;
             num_ranks[rank]++;
         }
-        int min_time = solve(num_pratas, num_ranks);
+        int min_time = solve(target, num_ranks);
         std::cout << min_time << std::endl;
     }
     return 0;
