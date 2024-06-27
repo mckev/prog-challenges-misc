892ccc69797b114b50cd4d13fb0bb1a975487899
Kevin Haritmonds <haritmonds@yahoo.com>
2024-06-27 23:29:25 +0530

    HackerRank : Sherlock and Anagrams

--- easy/20240625-hackerrank-sherlock-and-anagrams/main.cpp
+++ easy/20240625-hackerrank-sherlock-and-anagrams/main.cpp
@@ -1,26 +1,23 @@
+// https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem
+
 #include <cassert>
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
-
-std::map<char, int> calcFreq(const std::string& s, int i, int len) {
-    std::map<char, int> freq;
-    for (int l = 0; l < len; l++) {
-        freq[s[i + l]]++;
-    }
-    return freq;
-}
 
 int sherlockAndAnagrams(const std::string& s) {
     int count = 0;
-    for (int len = 1; len <= s.size(); len++) {
-        for (int i = 0; i + len <= s.size(); i++) {
-            std::map<char, int> freq1 = calcFreq(s, i, len);
-            for (int j = i + 1; j + len <= s.size(); j++) {
-                if (freq1 == calcFreq(s, j, len)) {
-                    count++;
-                }
+    std::vector<std::map<char, int>> freq(s.size(), std::map<char,int>());
+    for (int len = 0; len < s.size(); len++) {
+        for (int i = 0; i + len < s.size(); i++) {
+            char ch = s[i + len];
+            freq[i][ch]++;
+        }
+        for (int i = 0; i + len < s.size(); i++) {
+            for (int j = i + 1; j + len < s.size(); j++) {
+                if (freq[i] == freq[j]) count++;
             }
         }
     }
@@ -28,12 +25,12 @@ int sherlockAndAnagrams(const std::string& s) {
 }
 
 int main() {
-    // assert(sherlockAndAnagrams("mom") == 2);
-    // assert(sherlockAndAnagrams("abba") == 4);
-    // assert(sherlockAndAnagrams("abcd") == 0);
-    // assert(sherlockAndAnagrams("ifailuhkqq") == 3);
-    // assert(sherlockAndAnagrams("kkkk") == 10);
-    // assert(sherlockAndAnagrams("cdcd") == 5);
+    assert(sherlockAndAnagrams("mom") == 2);
+    assert(sherlockAndAnagrams("abba") == 4);
+    assert(sherlockAndAnagrams("abcd") == 0);
+    assert(sherlockAndAnagrams("ifailuhkqq") == 3);
+    assert(sherlockAndAnagrams("kkkk") == 10);
+    assert(sherlockAndAnagrams("cdcd") == 5);
 
     int t;
     std::cin >> t;
@@ -42,5 +39,6 @@ int main() {
         std::cin >> s;
         std::cout << sherlockAndAnagrams(s) << std::endl;
     }
+
     return 0;
 }
