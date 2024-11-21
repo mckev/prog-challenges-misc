b6d8069ee13d7db12d759a8f8fc90cb53bb84755
KHaritmonds <kharitmonds@slb.com>
2024-11-21 12:58:34 +0530

    LeetCode : Power of Two

--- easy/20241120-leetcode-power-of-two/main.cpp
+++ easy/20241120-leetcode-power-of-two/main.cpp
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/power-of-two/description/
 
 #include <cassert>
+#include <cmath>
 
 
-class Solution {
+class SolutionBitwise {
 public:
     bool isPowerOfTwo(long long n) {
         return (n != 0) && !(n & (n - 1));
@@ -11,7 +12,24 @@ public:
 };
 
 
-class Solution2 {
+class SolutionMath {
+public:
+    const double EPSILON = 0.0000000001;
+
+    bool isPowerOfTwo(long long n) {
+        if (n <= 0) return false;
+        // n = 2^p
+        // log n = p log 2
+        // p = log n / log 2
+        double p = std::log(n) / std::log(2);
+        // Check if p is an integer
+        bool isInteger = std::abs(p - int(p)) < EPSILON;
+        return isInteger;
+    }
+};
+
+
+class SolutionRecursive {
 public:
     bool isPowerOfTwo(long long n) {
         if (n == 0) return false;
@@ -23,12 +41,22 @@ public:
 
 
 int main() {
+    {
+        SolutionMath solutionMath = SolutionMath();
+        assert(solutionMath.isPowerOfTwo(536870912) == true);
+        assert(solutionMath.isPowerOfTwo(16777217) == false);
+        assert(solutionMath.isPowerOfTwo(0) == false);
+        assert(solutionMath.isPowerOfTwo(-16) == false);
+    }
     for (int n = -1000000; n < 1000000; n++) {
-        Solution solution = Solution();
-        Solution2 solution2 = Solution2();
-        bool answer = solution.isPowerOfTwo(n);
-        bool answer2 = solution2.isPowerOfTwo(n);
-        assert(answer == answer2);
+        SolutionBitwise solutionBitwise = SolutionBitwise();
+        SolutionMath solutionMath = SolutionMath();
+        SolutionRecursive solutionRecursive = SolutionRecursive();
+        bool answerBitwise = solutionBitwise.isPowerOfTwo(n);
+        bool answerMath = solutionMath.isPowerOfTwo(n);
+        bool answerRecursive = solutionRecursive.isPowerOfTwo(n);
+        assert(answerBitwise == answerRecursive);
+        assert(answerMath == answerRecursive);
     }
     return 0;
 }
