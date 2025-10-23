289f0c907f9469466deb2d1ba0570d4f4d820e5e
KHaritmonds <kharitmonds@slb.com>
2025-10-23 16:07:16 +0530

    Codeforces : Array

--- easy/20251003-codeforces-array/main.cpp
+++ easy/20251003-codeforces-array/main.cpp
@@ -35,7 +35,7 @@ long long solve_bruteforce(int n) {
 
 
 class Combinatorics {
-// To calculate C(n, r) mod p =  (n! / (r!  (n - r)!) ) mod p
+// To calculate C(n, r) mod p =  ( n! / (r!  (n - r)!) ) mod p
 private:
     static long long modpow(long long base, long long exp, long long mod) {
         // Compute (base^exp) % mod using binary exponentiation
@@ -51,24 +51,24 @@ private:
         return result;
     }
 
-    int n;
+    int max_n;
     int mod;
     std::vector<long long> factorial, inverse_factorial;
 
 public:
-    Combinatorics(int n, int mod) : n(n), mod(mod), factorial(n + 1), inverse_factorial(n + 1) {
+    Combinatorics(int max_n, int mod) : max_n(max_n), mod(mod), factorial(max_n + 1), inverse_factorial(max_n + 1) {
         factorial[0] = 1;
-        for (int i = 1; i <= n; i++) factorial.at(i) = factorial.at(i - 1) * i % mod;
+        for (int i = 1; i <= max_n; i++) factorial.at(i) = (factorial.at(i - 1) * i) % mod;
         // Using Fermat's Little Theorem if p is prime:  a^(-1) mod p = a^(p - 2) mod p
-        inverse_factorial.at(n) = modpow(factorial.at(n), mod - 2, mod);
-        for (int i = n - 1; i >= 0; i--) inverse_factorial.at(i) = inverse_factorial.at(i + 1) * (i + 1) % mod;
+        inverse_factorial.at(max_n) = modpow(factorial.at(max_n), mod - 2, mod);
+        for (int i = max_n - 1; i >= 0; i--) inverse_factorial.at(i) = (inverse_factorial.at(i + 1) * (i + 1)) % mod;
     }
 
     long long C(int n, int r) const {
+        assert (n <= max_n);
         // 0 <= r <= n, mod is a prime number
-        assert (0 <= r);
-        assert (r <= n);
-        return factorial.at(n) * inverse_factorial.at(r) % mod * inverse_factorial.at(n - r) % mod;
+        assert (0 <= r); assert (r <= n);
+        return ((factorial.at(n) * inverse_factorial.at(r)) % mod * inverse_factorial.at(n - r)) % mod;
     }
 };
 
