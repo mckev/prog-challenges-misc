7054e3fa61f9ad9c9df704210e42462940bde969
KHaritmonds <kharitmonds@slb.com>
2025-12-24 14:23:05 +0530

    AtCoder : Knapsack 2

--- easy/20251224-atcoder-knapsack-2/main.cpp
+++ easy/20251224-atcoder-knapsack-2/main.cpp
@@ -1,9 +1,11 @@
-// https://atcoder.jp/contests/dp/tasks/dp_d
+// https://atcoder.jp/contests/dp/tasks/dp_e
+// Hint: https://www.youtube.com/live/FAQxdm0bTaw?t=1668
 
 #include <cassert>
 #include <iostream>
 #include <random>
 #include <vector>
+#pragma GCC optimize "trapv"
 
 
 struct Item {
@@ -47,14 +49,22 @@ public:
 
 class SolutionEfficient {
 public:
+    long long INF = std::numeric_limits<long long>::max() - (1000000000 * 100);
     long long solve(const std::vector<Item>& items, const int max_weight) {
-        std::vector<long long> dp(max_weight + 1);                  // dp[] tracks the max value on a given weight
+        int max_value = 0; for (const Item& item : items) max_value += item.value;
+        std::vector<long long> dp(max_value + 1, INF);                  // dp[] tracks the minimum weight to achieve a value
+        dp[0] = 0;
         for (const Item& item : items) {
-            for (int w = max_weight; w >= item.weight; w--) {
-                dp.at(w) = std::max(dp.at(w), dp.at(w - item.weight) + item.value);
+            for (int v = max_value; v >= item.value; v--) {
+                dp.at(v) = std::min(dp.at(v), dp.at(v - item.value) + item.weight);
             }
         }
-        return dp.at(max_weight);
+        for (int v = max_value; v >= 0; v--) {
+            if (dp.at(v) <= max_weight) {
+                return v;
+            }
+        }
+        assert(false);
     }
 };
 
@@ -69,12 +79,12 @@ void test() {
         assert(solution_efficient.solve(items, max_weight) == 90);
     }
     {
-        std::vector<Item> items = { {0, 1, 1000000000}, {1, 1, 1000000000}, {2, 1, 1000000000}, {3, 1, 1000000000}, {4, 1, 1000000000} };
-        int max_weight = 5;
+        std::vector<Item> items = { {0, 1000000000, 10} };
+        int max_weight = 1000000000;
         SolutionBruteforce solution_bruteforce = SolutionBruteforce();
-        assert(solution_bruteforce.solve(items, max_weight) == 5000000000);
+        assert(solution_bruteforce.solve(items, max_weight) == 10);
         SolutionEfficient solution_efficient = SolutionEfficient();
-        assert(solution_efficient.solve(items, max_weight) == 5000000000);
+        assert(solution_efficient.solve(items, max_weight) == 10);
     }
     {
         std::vector<Item> items = { {0, 6, 5}, {1, 5, 6}, {2, 6, 4}, {3, 6, 6}, {4, 3, 5}, {5, 7, 2} };
@@ -85,30 +95,10 @@ void test() {
         assert(solution_efficient.solve(items, max_weight) == 17);
     }
     {
-        // Kevin
-        std::vector<Item> items = { {0, 220, 170}, {1, 84, 19}, {2, 876, 56}, {3, 965, 261}, {4, 956, 49}, {5, 298, 538} };
-        int max_weight = 952;
-        SolutionBruteforce solution_bruteforce = SolutionBruteforce();
-        assert(solution_bruteforce.solve(items, max_weight) == 727);
-        SolutionEfficient solution_efficient = SolutionEfficient();
-        assert(solution_efficient.solve(items, max_weight) == 727);
-    }
-    {
-        // Kevin
-        std::vector<Item> items = { {0, 866, 24}, {1, 159, 714}, {2, 39, 549}, {3, 420, 657}, {4, 662, 794}, {5, 375, 726}, {6, 593, 999}, {7, 588, 58}, {8, 140, 578}, {9, 265, 189}, {10, 163, 677}, {11, 131, 560}, {12, 768, 397}, {13, 151, 568}, {14, 628, 184}, {15, 820, 340} };
-        int max_weight = 615;
-        SolutionBruteforce solution_bruteforce = SolutionBruteforce();
-        long long answer_bruteforce = solution_bruteforce.solve(items, max_weight);
-        assert(answer_bruteforce == 2537);
-        SolutionEfficient solution_efficient = SolutionEfficient();
-        long long answer_efficient = solution_efficient.solve(items, max_weight);
-        assert(answer_efficient == 2537);
-    }
-    {
         std::random_device rd;
         std::mt19937 mt(rd());
         std::uniform_real_distribution<> rnd(0, 1);             // [0, 1)
-        for (int t = 0; t < 100000; t++) {
+        for (int t = 0; t < 1000; t++) {
             std::vector<Item> items;
             int N = int(rnd(mt) * 20) + 1;
             for (int n = 0; n < N; n++) {
@@ -129,7 +119,7 @@ void test() {
 
 
 int main() {
-    test();
+    // test();
     int N; std::cin >> N;
     int max_weight; std::cin >> max_weight;
     std::vector<Item> items;
